e78c54b9647c9639bfd6476cceadbc71aaa619ad
Abdalla Abdulmonem <abdallag@google.com>
2019-02-08 19:45:24 -0800

    Updates to local agent

--- nova-local-agent.sh
+++ nova-local-agent.sh
@@ -1,61 +1,42 @@
 #!/bin/bash -v
-export GCS_BUCKET=gs://nova-artifacts-$(gcloud config get-value project)
-export ZONE=$(gcloud config get-value compute/zone)
-if [[ ZONE == "(unset)" ]]; then
-  INSTANCE_ZONE="/"$(curl http://metadata.google.internal/computeMetadata/v1/instance/zone -H "Metadata-Flavor: Google")
-  INSTANCE_ZONE="${INSTANCE_ZONE##/*/}"
-fi
+sourc utils.sh
 
 gsutil ls ${GCS_BUCKET} ||  gsutil mb ${GCS_BUCKET}
-
 gsutil ls ${GCS_BUCKET}/jobs ||  gsutil mkdir $GCS_BUCKET/jobs
 
-cd /home/jupyter
-ls jobs || mkdir jobs
-
-function get-job-yaml-val() {
-  export key=$1:
-  export val=$(grep ${key} "jobs/$2.yaml")
-  echo ${val:${#key}:${#val}}
-}
-
-function get-image() {
-  hostname | grep pytorch
-  if [[ "$?" == "0" ]]; then
-    echo pytorch-latest-gpu
-  else
-    echo tf-latest-gpu
-  fi
-}
+cd ${JUPYTER_HOME}
+[[ -e jobs ]] || mkdir jobs
 
 while :
 do
   sleep 5
-  ls jobs/*.yaml || continue
+  [[ -e jobs/*.yaml ]] || continue
   echo "Jobs found."
-  for job in jobs/*.yaml
+  for jobfile in jobs/*.yaml
   do
-    export job=${job:5:-5}
+    export job=${jobfile:5:-5}
     echo "Processing job ${job}"
     gsutil ls ${GCS_BUCKET}/jobs/${job}
     if [[ "$?" != "0" ]]; then
       echo "Creating job ${job}"
-      export gput=$(get-job-yaml-val gpu_type ${job})
+      export gput=$(get-yaml-val gpu_type ${jobfile})
       echo $gput
-      export gpuc=$(get-job-yaml-val gpu_count ${job})
+      export gpuc=$(get-yaml-val gpu_count ${jobfile})
       echo $gpuc
-      export mcount=$(get-job-yaml-val machine_count ${job})
+      export mcount=$(get-yaml-val machine_count ${jobfile})
       echo $mcount
-      export dir=$(get-job-yaml-val dir ${job})
+      export dir=$(get-yaml-val dir ${job})
       echo $dir
-      export mtype=$(get-job-yaml-val machine_type ${job})
+      export mtype=$(get-yaml-val machine_type ${jobfile})
       echo $mtype
+      export zone=$(get-yaml-val zone ${jobfile})
+      [[ -z "$zone" ]] || export ZONE=$zone
       gsutil mkdir $GCS_BUCKET/jobs/$job
       for i in $(seq 1 1 $mcount); do
         export machine=$job$i
         echo "Creating VM: $machine"
         gcloud compute instances create $machine \
-          --zone=$(gcloud config get-value compute/zone) \
+          --zone=$ZONE \
           --image-family=$(get-image) \
           --image-project=deeplearning-platform-release \
           --maintenance-policy=TERMINATE \
@@ -66,7 +47,8 @@ do
 
         export machine_dir=$GCS_BUCKET/jobs/$job/$machine
         gsutil mkdir $machine_dir
-        gsutil cp -r $dir $machine_dir/$(basename $dir)
+        gsutil mkdir $machine_dir/homedir
+        gsutil cp -r $dir/* $machine_dir/homedir
         gsutil cp jobs/$job.yaml $machine_dir/$job.yaml
       done
     fi
